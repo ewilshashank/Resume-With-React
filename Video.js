d836bd754bef94d2b75c773bc1e52dd52291ec51
Code With Ewil <93051565+ewilshashank@users.noreply.github.com>
2023-07-08 11:57:24 +0530

    Update Video.js

--- Video.js
+++ Video.js
@@ -1,20 +1,29 @@
-import React from "react";
+export default function Video({ title, interest, verified }) {
+  let nameJSX;
+  if (verified) {
+    // nameJSX = <div>I complete My Resume ✅</div>;
+  } else {
+    nameJSX = <div>I complete My Resume</div>;
+  }
 
-export default function Video({title, interest}) {
   return (
-  <div>
-    <Interest title={title} interest={interest}></Interest>
-    <Skills></Skills>
-  </div>
-    );
+    <div>
+      <Interest title={title} interest={interest} verified={true}></Interest>
+      <Skills></Skills>
+      {nameJSX}
+    </div>
+  );
 }
 
-function Interest({ title, interest }) {
+function Interest({ title, interest, verified }) {
   return (
     <>
       <div>
         <h2>{title}</h2>
         <ul>{interest}</ul>
+        {/* <div>I complete My Resume {verified ? '✅': ''}</div> */}
+        <div>I complete My Resume {verified && "✅"}</div>{" "}
+        {/* only boolean work */}
       </div>
     </>
   );
@@ -24,8 +33,12 @@ function Skills() {
   let obj = {
     name: "skills",
     haveSkills: ["Master in Web Development", "Master In Web Design"],
-    title: 'Education', 
-    studyIn:['Amity InterNational School','Dream Zone Institue Of Technology','Chandigarh University']
+    title: "Education",
+    studyIn: [
+      "Amity InterNational School",
+      "Dream Zone Institue Of Technology",
+      "Chandigarh University"
+    ]
   };
 
   return (
@@ -33,55 +46,68 @@ function Skills() {
       <div>
         <h2>{obj.name}</h2>
         <ul>
-          {obj.haveSkills.map((item,index) => (
+          {obj.haveSkills.map((item, index) => (
             <li key={index}>{item}</li>
           ))}
         </ul>
       </div>
-      <Education title={obj.title} studyIn={obj.studyIn.map((item, index) => (
+      <Education
+        title={obj.title}
+        studyIn={obj.studyIn.map((item, index) => (
           <li key={index}>{item}</li>
-      ))} />
+        ))}
+      />
     </>
   );
 }
 
-function Education({title, studyIn}){
-
+function Education({ title, studyIn }) {
   let obj = {
     experience: "Self Experianece",
     projects: "40+ Self Made"
-  }
+  };
 
-  return(
+  return (
     <>
       <div>
-          <h2>{title}</h2>
-          <ul>
-            {studyIn}
-            </ul>
+        <h2>{title}</h2>
+        <ul>{studyIn}</ul>
       </div>
-      <Experience selfExp={obj.experience} madeProjects={obj.projects}/>
+      <Experience selfExp={obj.experience} madeProjects={obj.projects} />
     </>
   );
 }
 
-function Experience(props){
-return(
-  <>
-    <div>
-      <h1>Experience</h1>
-      <ul>
-        <li>{props.selfExp}</li>
-        <li>{props.madeProjects}</li>
+function Experience(props) {
+  return (
+    <>
+      <div>
+        <h1>Experience</h1>
+        <ul>
+          <li>{props.selfExp}</li>
+          <li>{props.madeProjects}</li>
         </ul>
       </div>
       <Footer />
-  </>
-)
+    </>
+  );
 }
 
-function Footer(props){
-  return(
-    <footer style={{backgroundColor : 'grey', padding:'20px 10px', color:'white'}}><p><i><b style={{float : 'right'}}>Shashank singh</b></i></p></footer>
-  )
+function Footer(props) {
+  const handlePDF = () => {
+    window.print();
+  };
+
+  return (
+    <footer
+      style={{ backgroundColor: "grey", padding: "20px 10px", color: "white" }}
+    >
+      <button onClick={handlePDF}></button>
+      <p>
+        <i>
+          <b style={{ float: "right" }}>Shashank singh</b>
+        </i>
+      </p>
+    </footer>
+  );
 }
